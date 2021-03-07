808d426ee868efab7d56f50419d49f57465e9d28
Marcelo Chaves <marcelochaves17@gmail.com>
2021-03-07 17:08:46 -0300

    Remove namespaces

--- Assets/src/Editor/GraphEditor.cs
+++ Assets/src/Editor/GraphEditor.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using UnityEngine;
 using UnityEditor;
+using UnityEngine;
 
 namespace NodeGraph
 {
