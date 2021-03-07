20193f3cfbb6bf8687412ca911240861bb0b58e5
Marcelo Chaves <marcelochaves17@gmail.com>
2021-03-07 02:06:36 -0300

    Update EditorNodeRenderData struct

---
+++ Assets/src/Editor/EditorNodeRenderData.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace NodeGraph
+{
+    public struct EditorNodeRenderData
+    {
+        public Rect NodeRect;
+        public float PinVerticalOffset;
+        public float InputPinHorizontalOffset;
+        public float OutputPinHorizontalOffset;
+        public float PinVerticalSpacing;
+        public float PinSize;
+    }
+}
