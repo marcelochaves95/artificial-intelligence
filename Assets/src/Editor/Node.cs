d97dd19422ce2c30aab25766c69543f920601f50
Marcelo Chaves <marcelochaves17@gmail.com>
2021-03-07 02:10:40 -0300

    Update Node class

--- Assets/src/Editor/Node.cs
+++ Assets/src/Editor/Node.cs
@@ -1,33 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Reflection;
 
-// TODO:
-// Create editor variant of node, renders GL to the editor window
-// can be dragged around in the editor window
-// exports its data to Node class
-
-[System.Serializable]
-public class Pin
+namespace NodeGraph
 {
-	[SerializeField] public string Name;
-	[SerializeField] public string Type;
-	[SerializeField] public Pin Next;
-}
-
-[System.Serializable]
-public class Node
-{
-
-	[SerializeField] public string ClassName { get; set; }
-	[SerializeField] public string FunctionName { get; set; }
-	[SerializeField] private Node NextFlowNode;
-	[SerializeField] private List<Pin> OutputPins;
-
-	public void Evaluate()
+	// TODO:
+	// Create editor variant of node, renders GL to the editor window
+	// can be dragged around in the editor window
+	// exports its data to Node class
+	[Serializable]
+	public class Node
 	{
+		[SerializeField]
+		public string ClassName { get; set; }
+		[SerializeField]
+		public string FunctionName { get; set; }
 
+		[SerializeField]
+		private Node _nextFlowNode;
+		[SerializeField]
+		private List<Pin> _outputPins;
+
+		public void Evaluate()
+		{
+			//
+		}
 	}
-
 }
